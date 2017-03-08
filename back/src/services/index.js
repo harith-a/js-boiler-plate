c665b5752423f24577500bc506fc76e3f995ee26
hyxer <harith87@me.com>
2017-03-08 15:33:49 +0800

    init

---
+++ back/src/services/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const upload = require('./upload');
+const authentication = require('./authentication');
+const user = require('./user');
+
+module.exports = function() {
+  const app = this;
+
+
+  app.configure(authentication);
+  app.configure(user);
+  app.configure(upload);
+};
---
+++ back/src/services/upload/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const path = require('path');
+const NeDB = require('nedb');
+const service = require('feathers-nedb');
+const hooks = require('./hooks');
+
+module.exports = function(){
+  const app = this;
+
+  const db = new NeDB({
+    filename: path.join(app.get('nedb'), 'uploads.db'),
+    autoload: true
+  });
+
+  let options = {
+    Model: db,
+    paginate: {
+      default: 5,
+      max: 25
+    }
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/uploads', service(options));
+
+  // Get our initialize service to that we can bind hooks
+  const uploadService = app.service('/uploads');
+
+  // Set up our before hooks
+  uploadService.before(hooks.before);
+
+  // Set up our after hooks
+  uploadService.after(hooks.after);
+};
